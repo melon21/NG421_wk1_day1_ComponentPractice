797781d51f0e356099f298a40ef997af706267a1
Melinda Marino <melindamarino22@gmail.com>
2020-04-05 15:40:35 -0500

    updates on pt 2

--- src/app/tasks-panel/tasks-panel.component.ts
+++ src/app/tasks-panel/tasks-panel.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import tasks from 'src/app/tasks';
 @Component({
   selector: 'app-tasks-panel',
   templateUrl: './tasks-panel.component.html',
   styleUrls: ['./tasks-panel.component.css']
 })
 export class TasksPanelComponent implements OnInit {
-
+  tasks:any[]= [];
   constructor() { }
 
   ngOnInit() {
--- src/app/transactions-panel/transactions-panel.component.ts
+++ src/app/transactions-panel/transactions-panel.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import orders from "src/app/orders";
 
 @Component({
+  
   selector: 'app-transactions-panel',
   templateUrl: './transactions-panel.component.html',
   styleUrls: ['./transactions-panel.component.css']
 })
 export class TransactionsPanelComponent implements OnInit {
-
+  orders:any[]= [];
   constructor() { }
 
   ngOnInit() {
+   this.orders= orders;
   }
 
 }
